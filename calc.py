4660bbcde1ee225f99e3284ce4625e7314b3cc7f
mmshenoy <msmohit004@gmail.com>
2013-06-09 15:23:30 -0700

    test 1 complete

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 class Word:
 	
@@ -17,8 +18,8 @@ class Word:
 		self.instances = instances
 	
 def main():
-    song_file = open(sys.argv[1])
-    corpus_file = open(sys.argv[2])
+    dir_name = 'D:\\Code\\GitHub\\sentiment\\song_files'
+    corpus_file = open(sys.argv[1])
     scores = {}
     song_score = 0
     song_words = 0
@@ -28,26 +29,37 @@ def main():
 		word_obj = Word()
 		word_obj.setWord(score,-1)
 		scores[term] = word_obj
+
+    write_file = open('output.csv','wb')
 	
-    for line in song_file:
-		if len(line) > 0:
-			#print line
-			words = line.split()
-			line_score = 0
-			line_words = len(words)
-			for word in words:
-				#print word
-				if scores.has_key(word):
-					#print scores[word].getVal()
-					line_score += scores[word].getVal()
-			print 'Line Avg: ' + str(float(line_score)/line_words)
-			song_score += line_score
-			song_words += line_words
-	
-    print 'Song Score: ' + str(song_score) + '\nSong Avg: ' + str(float(song_score)/song_words)
+    for files in os.listdir(dir_name):
+		print files
+		song_file = open(dir_name + '\\' + files)
+		for line in song_file:
+			if len(line) > 0:
+				#print line
+				words = line.split()
+				line_score = 0
+				line_words = len(words)
+				for word in words:
+					#print word
+					if scores.has_key(word):
+						#print scores[word].getVal()
+						line_score += scores[word].getVal()
+				#print 'Line Avg: ' + str(float(line_score)/line_words)
+				song_score += line_score
+				song_words += line_words
 		
-	
+		song_avg = float(song_score)/song_words
+		print 'Song Score: ' + str(song_score) + '\nSong Avg: ' + str(song_avg)
+		objs = files.split('_')
+		band = objs[0]
+		year = objs[1]
+		song = objs[2].split('.')[0]
+		row = band + ',' + song + ',' + year + ',' + str(song_score) + ',' + str(song_avg)
+		write_file.write(row)
 		
-			
+    write_file.close()
+	
 if __name__ == '__main__':
     main()
